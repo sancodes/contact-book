a9a6d124274ea5d783036cabc11c3afbdf97e26b
sancodes <munsan14@gmail.com>
2021-03-03 14:51:22 -0800

    add path to db connection and rename variable

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+require('./config/database');
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let contactsRouter = require('./routes/contact');
 
 let app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/contacts', contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
