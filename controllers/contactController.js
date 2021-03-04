1e2fd1b8d1ed265807d263a4c0ca6bf9a7c188f5
sancodes <munsan14@gmail.com>
2021-03-04 00:57:26 -0800

    add logic to the server path, -> debug getPersonDetails

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ let ContactModel = require('../models/contactModel');
 module.exports.getAllContacts = async (req, res, next) => {
     let contactList = await ContactModel.find({});
    
-    res.render('index', {
+    res.render('home', {
         title: 'Contacts Home Page',
         contact: contactList
     });
@@ -12,11 +12,33 @@ module.exports.getAllContacts = async (req, res, next) => {
 
 
 //GET --> new contact form
+module.exports.addNewContactForm = (req, res, next) => {
+    res.render('new', {
+      title: 'Add Contact'
+  })  
+};
 
 //POST --> save contact in the database
+module.exports.addNewContact = async (req, res, next) => {
+    try {
+        let newContact = new ContactModel(req.body);
+        await newContact.save();
+        res.redirect('/contacts');
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 //GET  --> specific person's detail
-
+module.exports.getPersonDetails = async (req, res, next) => {
+    try {
+        let personDetails = await ContactModel.findById(req.params.id);
+        console.log(personDetails);
+        res.render('person');
+    } catch (err) {
+        console.log(err);
+    }
+};
 //DELETE  --> person contact from db
 
 //UPDATE --> person 
