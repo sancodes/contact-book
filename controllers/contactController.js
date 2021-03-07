3541c583739a5e5442947bcd8a211f45cb4814c1
sancodes <munsan14@gmail.com>
2021-03-06 21:58:10 -0800

    fix issue with addressSchema not retreiving value from HTML form

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,7 +21,13 @@ module.exports.addNewContactForm = (req, res, next) => {
 //POST --> save contact in the database
 module.exports.addNewContact = async (req, res, next) => {
     try {
+        //apparently only pushes the contactSchema field leaves addressSchema field empty
         let newContact = new ContactModel(req.body);
+        console.log('before address: ' + newContact);
+        //since addressSchema field is empty, I had to push the address field again
+        newContact.address.push(req.body);
+        console.log('after address: ' + newContact);
+
         await newContact.save();
         res.redirect('/contacts');
     } catch (err) {
