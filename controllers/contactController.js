ee7223ec72476bf2fb1d8acf93476bc73f99870e
sancodes <munsan14@gmail.com>
2021-03-12 10:05:27 -0800

    add logic to updating details, still need work on validation - not complete

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -25,9 +25,12 @@ module.exports.addNewContact = async (req, res, next) => {
         let newContact = new ContactModel(req.body);
         console.log('before address: ' + newContact);
         //since addressSchema field is empty, I had to push the address field again
-        newContact.address.push(req.body);
+        //but only push if the address is not empty(undefined) --for initial check
+        //this check stop from putting that empty fields in the address 
+        if (!(req.body.address === undefined)) {
+            newContact.address.push(req.body);
+        }
         console.log('after address: ' + newContact);
-
         await newContact.save();
         res.redirect('/contacts');
     } catch (err) {
@@ -39,6 +42,7 @@ module.exports.addNewContact = async (req, res, next) => {
 module.exports.getPersonDetails = async (req, res, next) => {
     try {
         let personDetails = await ContactModel.findById(req.params.id);
+        console.log("Personal Details" + personDetails);
         res.render('person', {
             title: 'Detail Page',
             personInfo: personDetails
@@ -47,6 +51,7 @@ module.exports.getPersonDetails = async (req, res, next) => {
         console.log(err);
     }
 };
+
 //DELETE  --> person contact from db
 module.exports.deletePerson = async (req, res, next) => {
     try {
@@ -56,7 +61,6 @@ module.exports.deletePerson = async (req, res, next) => {
         console.log(err);
     }
 }
-//UPDATE --> person 
 
 //GET --> add another address
 module.exports.getAdditionalAddressForm = async (req, res, next) => {
@@ -82,6 +86,71 @@ module.exports.addAdditionalAddress = async (req, res, next) => {
         await personDetails.save(); //since the personDetails has push the new address, need to save it to the db 
         res.redirect(`/contacts/person/${personDetails._id}`);
 
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//GET --> personDetails Update Form
+module.exports.getUpdateContactDetails = async(req, res, next) => {
+    try {
+        let personDetails = await ContactModel.findById(req.params.id);
+        res.render('updateDetail', {
+            title: 'Update Detail',
+            personInfo: personDetails
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//PUT --> 
+module.exports.updateContactDetails = async (req, res, next) => {
+    try {
+        console.log('inside the controller!');
+        console.log(req.body);  //i need to catch the data and compare it with my stored data and change it
+        let actualPerson = await ContactModel.findById(req.body._id);
+        // console.log(actualPerson);
+
+        //maybe boolean to keep track if there was any changes or not? 
+        //maybe i can even add counter to see how many items were changed;
+        // await actualPerson.save();
+        if (!(req.body === undefined)) {
+            if (req.body.firstName !== actualPerson.firstName) {
+                actualPerson.firstName = req.body.firstName;
+            }
+            if (req.body.lastName !== actualPerson.lastName) {
+                actualPerson.lastName = req.body.lastName;
+            }
+            if (req.body.phoneNumber !== actualPerson.phoneNumber) {
+                actualPerson.phoneNumber = req.body.phoneNumber;
+            }
+            //if the email exists in the actual db then we can worry about changing, if not it wont even show in the update form field
+            if (actualPerson.email) {
+                if (req.body.email !== actualPerson.email) {
+                    actualPerson.email = req.body.email;
+                }
+            }
+            //if the address exists in the actual db then we can worry about changing, if not it wont even show in the update form field
+            //so the update form passes in a list of street, state and zip, not like a object/dictionary
+            if (!(actualPerson.address === undefined)) {
+                for (let i = 0; i < actualPerson.address.length; i++) {
+                    if (actualPerson.address[i].street !== req.body.street[i]) {
+                        actualPerson.address[i].street = req.body.street[i];  //now the part that is not working is if there's only one address, then the req.body.street[i] will only take the first lettter. since the req.body.street at that point will not have the address as an list so that's why.
+                    }
+                    if (actualPerson.address[i].state !== req.body.state[i]) {
+                        console.log(req.body.state[i]);
+                        actualPerson.address[i].state = req.body.state[i];
+                    }
+                    //gotta make sure they actually enter correct format zipcode
+                    if (actualPerson.address[i].zip !== req.body.zip[i]) {
+                        actualPerson.address[i].zip = req.body.zip[i];
+                    }
+                }
+            };
+        }
+        await actualPerson.save();
+        res.redirect(`/contacts/person/${actualPerson._id}`);
     } catch (err) {
         console.log(err);
     }
