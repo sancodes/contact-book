bd85bf0adfa3ac3523179cd999bfae32ab83622a
sancodes <munsan14@gmail.com>
2021-03-24 14:04:10 -0700

    fix bugs -- when there's only one address, user is still able to modify and save to db

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -132,7 +132,6 @@ module.exports.getUpdateContactDetails = async(req, res, next) => {
 //PUT --> 
 module.exports.updateContactDetails = async (req, res, next) => {
     try {
-        console.log('inside the controller!');
         let formInputs = JSON.parse(JSON.stringify(req.body));
         let actualPerson = await ContactModel.findById(formInputs._id);
 
@@ -157,36 +156,38 @@ module.exports.updateContactDetails = async (req, res, next) => {
             }
             //if the address exists in the actual db then we can worry about changing, if not it wont even show in the update form field
             //so the update form passes in a list of street, state and zip, not like a object/dictionary
+            //whatever is being passed form the form changes from string to array: 
+            // like if only address is there, the field is a string, 
+            //but
+            //if there more than one address, then the field is passed in as a array
             if (!(actualPerson.address == undefined)) {
-                for (let i = 0; i < actualPerson.address.length; i++) {
-                    if (actualPerson.address[i].street !== formInputs.street) {
-                        actualPerson.address[i].street = formInputs.street;  //now the part that is not working is if there's only one address, then the req.body.street[i] will only take the first lettter. since the req.body.street at that point will not have the address as an list so that's why.
+                if (actualPerson.address.length === 1) {
+                    console.log('reached to the point where the address lenght is === 1');
+                    if (actualPerson.address[0].street !== formInputs.street) {
+                        actualPerson.address[0].street = formInputs.street;  //now the part that is not working is if there's only one address, then the req.body.street[i] will only take the first lettter. since the req.body.street at that point will not have the address as an list so that's why.
                     }
-                    if (actualPerson.address[i].state !== formInputs.state) {
-                        actualPerson.address[i].state = formInputs.state;
+                    if (actualPerson.address[0].state !== formInputs.state) {
+                        actualPerson.address[0].state = formInputs.state;
                     }
                     //gotta make sure they actually enter correct format zipcode
-                    if (actualPerson.address[i].zip !== formInputs.zip) {
-
-                        actualPerson.address[i].zip = formInputs.zip;
-                        //removing the field right away if null, else, change with the new input
-                        // if (formInputs.zip === null) {
-                        //     delete actualPerson.address[i]['zip'];
-                        // } else {
-                        //     actualPerson.address[i].zip = formInputs.zip;
-                        // }
+                    if (actualPerson.address[0].zip !== formInputs.zip) {
+                        actualPerson.address[0].zip = formInputs.zip;
+                    }
+                } else {
+                    console.log('reached to the point where the address lenght is > 1');
+                    for (let i = 0; i < actualPerson.address.length; i++) {
+                        if (actualPerson.address[i].street !== formInputs.street[i]) {
+                            actualPerson.address[i].street = formInputs.street[i]; 
+                        }
+                        if (actualPerson.address[i].state !== formInputs.state[i]) {
+                            actualPerson.address[i].state = formInputs.state[i];
+                        }
+                        if (actualPerson.address[i].zip !== formInputs.zip[i]) {
+                            actualPerson.address[i].zip = formInputs.zip[i];
+                        }
                     }
                 }
             }
-
-            // //double check to see if there are any address field with id only
-            // for (let i = 0; i < actualPerson.address.length; i++) {
-            //     //removing the id from deleted contact address, if everything is null dont want to leave the id hanging and take upspace
-            //     if (actualPerson.address[i].street === undefined && actualPerson.address[i].street === undefined && actualPerson.address[i].street === undefined) {
-            //         actualPerson.address[i].pop();
-            //     }
-            // }
-
         }
         await actualPerson.save();
         res.redirect(`/contacts/person/${actualPerson._id}`);
