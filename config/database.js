10319e9aaafde9349916c3337876a81fc8089aaf
sancodes <munsan14@gmail.com>
2021-10-08 14:24:29 -0700

    add async/await for db connection

--- config/database.js
+++ config/database.js
@@ -3,14 +3,22 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    }
+    catch (err) {
+        console.log(`Failed to connect to MongoDB`, err);
+    }
 
+}
+connectDB();
 const db = mongoose.connection;
 db.on('open', () => {
-    console.log('Connected to MongoDB database');
+    console.log(`Connected to MongoDB database on port ${db.port}`);
 });
 
